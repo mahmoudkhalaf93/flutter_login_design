a2eeb5bc8a1390e18fbad01d98d83389b40a7a5a
mahmoudkhalaf93 <modyelsayd54@gmail.com>
2021-09-17 14:18:02 +0200

    master test

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -133,40 +133,38 @@ class HomeScreen extends StatelessWidget {
                             SizedBox(
                               width: 8,
                             ),
-                            Expanded(
-                              child: ListView.builder(
-                                itemCount: 9,
-                                scrollDirection: Axis.horizontal,
-                                shrinkWrap: true,
-                                itemBuilder: (context, index) {
-                                  return Container(
-                                    width: 70,
-                                    margin:
-                                        EdgeInsets.symmetric(horizontal: 8),
-                                    alignment: Alignment.topCenter,
-                                    child: Column(
-                                      children: const [
-                                        CircleAvatar(
-                                          radius: 31,
-                                          backgroundImage: NetworkImage(
-                                              "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5bb22ae84bbe6f67d2e82e05%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D627%26cropX2%3D1639%26cropY1%3D129%26cropY2%3D1142"),
-                                        ),
-                                        SizedBox(
-                                          height: 8,
-                                        ),
-                                        Text(
-                                          "Mahmoud khalaf mohamed",
-                                          textAlign: TextAlign.center,
-                                          style:
-                                              TextStyle(color: Colors.white),
-                                          overflow: TextOverflow.ellipsis,
-                                          maxLines: 2,
-                                        ),
-                                      ],
-                                    ),
-                                  );
-                                },
-                              ),
+                            ListView.builder(
+                              itemCount: 9,
+                              scrollDirection: Axis.horizontal,
+                              shrinkWrap: true,
+                              itemBuilder: (context, index) {
+                                return Container(
+                                  width: 70,
+                                  margin:
+                                      EdgeInsets.symmetric(horizontal: 8),
+                                  alignment: Alignment.topCenter,
+                                  child: Column(
+                                    children: const [
+                                      CircleAvatar(
+                                        radius: 31,
+                                        backgroundImage: NetworkImage(
+                                            "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5bb22ae84bbe6f67d2e82e05%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D627%26cropX2%3D1639%26cropY1%3D129%26cropY2%3D1142"),
+                                      ),
+                                      SizedBox(
+                                        height: 8,
+                                      ),
+                                      Text(
+                                        "Mahmoud khalaf mohamed",
+                                        textAlign: TextAlign.center,
+                                        style:
+                                            TextStyle(color: Colors.white),
+                                        overflow: TextOverflow.ellipsis,
+                                        maxLines: 2,
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              },
                             ),
                           ],
                         ),
